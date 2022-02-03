fee1645fa0e269a93f6f5984aa6c8295eaa2abf0
Hammersmith888 <vladislavv8@mail.ru>
2022-02-03 20:56:06 +0300

    int project commit
    go to github

---
+++ Snowboard/Assets/CrashDitector.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDitector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Ground"){
+            crashEffect.Play();
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+    void ReloadScene(){
+        SceneManager.LoadScene(0);
+    }
+}
